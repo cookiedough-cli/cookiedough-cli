dc93c87a3b2094d363ed67d2f4cdb6ec565e13f3
abschill <ajay198810@gmail.com>
2022-05-13 14:55:37 -0600

    rework: remove commander add inquirer

--- src/internal/preset/node.ts
+++ src/internal/preset/node.ts
@@ -1,28 +1,48 @@
 export type NodePackagePreset = 'commonjs' | 'esm' | 'ts';
-export type NodeCompilerPreset = 'babel' | 'swc' | 'esbuild';
-export type NodeBundlerPreset = 'webpack' | 'esbuild' | 'rollup';
-export type NodeBuildPreset = 'esbuild' | 'gulp' | 'grunt' | 'make';
 export type NodePkgMgrPreset = 'npm' | 'yarn' | 'pnpm';
+export type NodeCompilerPreset = 'babel' | 'swc' | 'esbuild' | 'none';
+export type NodeBundlerPreset = 'webpack' | 'esbuild' | 'rollup' | 'snowpack' | 'none';
+export type NodeBuildPreset = 'esbuild' | 'gulp' | 'grunt' | 'make' | 'none(npm)';
+export const NodePkgPresets: NodePackagePreset[] = [
+	'commonjs', 'esm', 'ts'
+];
 
 export const NodePkgMgrPresets: NodePkgMgrPreset[] = [
-	'npm', 'yarn', 'pnpm'
+	'npm',
+	'yarn',
+	'pnpm'
 ];
 
 export const NodeBuildPresets: NodeBuildPreset[] = [
-	'make', 'gulp', 'grunt', 'esbuild'
+	'make',
+	'gulp',
+	'grunt',
+	'esbuild', 'none(npm)'
 ];
 
-export interface PackageConfig {
-	package_preset  : NodePackagePreset; //top level preset for the module generation
-	compiler_preset ?: NodeCompilerPreset; // which transpiler to install if any
-	bundler_preset  ?: NodeBundlerPreset; // which bundler to install if any
-	build_preset    ?: NodeBuildPreset; // which build system to set up if any
-	pkgr_preset     : NodePkgMgrPreset; // which process to install packages with
-}
+export const NodeCompilerPresets: NodeCompilerPreset[] = [
+	'babel', 'swc', 'esbuild', 'none'
+];
 
+export const NodeBundlerPresets: NodeBundlerPreset[] = [
+	'webpack', 'rollup', 'snowpack', 'esbuild', 'none'
+];
 
+// export interface PackageConfig {
+// 	package_preset  : NodePackagePreset; //top level preset for the module generation
+// 	compiler_preset ?: NodeCompilerPreset; // which transpiler to install if any
+// 	bundler_preset  ?: NodeBundlerPreset; // which bundler to install if any
+// 	build_preset    ?: NodeBuildPreset; // which build system to set up if any
+// 	pkgr_preset     : NodePkgMgrPreset; // which process to install packages with
+// }
 export function prompt_node(p: string, inquirer) {
 	inquirer.prompt([
+		{
+			type: 'list',
+			name: 'preset',
+			message: 'choose package preset',
+			choices: NodePkgPresets
+		},
 		{
 			type: 'list',
 			name: 'pkg_mgr',
@@ -34,6 +54,18 @@ export function prompt_node(p: string, inquirer) {
 			name: 'build_tools',
 			message: 'choose build environment',
 			choices: NodeBuildPresets
+		},
+		{
+			type: 'list',
+			name: 'compiler',
+			message: 'choose transpiler option',
+			choices: NodeCompilerPresets
+		},
+		{
+			type: 'list',
+			name: 'bundler',
+			message: 'choose bundler option',
+			choices: NodeBundlerPresets
 		}
 	]).then(answers => {
 		console.log(`node project at ${p}`);
--- src/util.ts
+++ src/util.ts
@@ -3,32 +3,7 @@
  */
 
 import { arch, platform, type } from 'os';
-const { log, error } = console;
-// import { verifiableTags } from './internal/tags';
-import { SystemOverview, TemplateName } from './types';
-
-
-/**
- *
- * @param i the user-defined string being matched up with the available tags
- * @returns a validated tag string from the enumerated types
- */
-// export const verifyTag = (
-// 	i: string,
-// 	throwable: boolean
-// ): TagType => {
-// 	// const match = verifiableTags.filter(tag => (tag.tag === i || tag.allowed_inputs.includes(i)));
-// 	// if(match.length === 0) {
-// 	// 	const err_msg = `language ${i} is not a valid configuration choice`;
-// 	// 	if(throwable) throw err_msg;
-// 	// 	error(err_msg);
-// 	// 	process.exit(1);
-// 	// }
-// 	// return <AllowedTag>match.shift().tag;
-// 	return 'golang';
-// };
-
-
+import { SystemOverview } from './types';
 /**
  *
  * @returns System Architecture Specs for setup process compatibility
@@ -41,18 +16,4 @@ SystemOverview {
         type: type(),
 		cwd: process.cwd()
     };
-}
-
-/**
- *
- * @param {ConfigWithoutSys} to_sys_stamp  the config prepared to get stamped by the system
- * @param {SysStamp} sysinfo stamp from get_sys context for the system that the program is running on
- * @returns A not-yet clean config template which has a valid tag, system info and the other uncleaned options for xConfig
- */
-function createConfig (
-	tag: TemplateName,
-	options: object // config parsed from inline user input in yargs
-) {
-	log(tag);
-    return {...options, tag, sys: getSysInfo()};
 }
