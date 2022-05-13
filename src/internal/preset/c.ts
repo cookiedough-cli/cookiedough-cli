2a1646aa8ffa0ec9f860d45b9e2f59f756da5692
abschill <ajay198810@gmail.com>
2022-05-13 14:59:14 -0600

    rework: remove commander add inquirer

--- src/internal/preset/c.ts
+++ src/internal/preset/c.ts
@@ -2,9 +2,16 @@ export type CPackagePreset = 'c' | 'cpp';
 
 export type CBuildPreset = 'make' | 'cmake';
 
-export type CPackageOptions = {
+export type CStandard = 'C99' | 'C11' | 'C17';
+export type CCStandard = 'C++98' | 'C++03' | 'C++11' | 'C++14' | 'C++17' | 'C++20';
 
-};
+export const CStandards: CStandard[] = [
+	'C99', 'C11', 'C17'
+];
+
+export const CCStandards: CCStandard[] = [
+	'C++98', 'C++03', 'C++11', 'C++14', 'C++17', 'C++20'
+];
 
 export const CCompilers = [
 	'gcc',
@@ -13,13 +20,18 @@ export const CCompilers = [
 ];
 
 export function prompt_c(p: string, inquirer) {
-
 	inquirer.prompt([
 		{
 			type: 'list',
 			name: 'compiler',
 			message: 'select a compiler',
 			choices: CCompilers
+		},
+		{
+			type: 'list',
+			name: 'standard',
+			message: 'select a standard',
+			choices: CStandards
 		}
 	]).then(answers => {
 		console.log(`c project at ${p}`);
@@ -27,13 +39,18 @@ export function prompt_c(p: string, inquirer) {
 	});
 }
 export function prompt_cpp(p: string, inquirer) {
-
 	inquirer.prompt([
 		{
 			type: 'list',
 			name: 'compiler',
 			message: 'select a compiler',
 			choices: CCompilers
+		},
+		{
+			type: 'list',
+			name: 'standard',
+			message: 'select a standard',
+			choices: CCStandards
 		}
 	]).then(answers => {
 		console.log(`c++ project at ${p}`);
