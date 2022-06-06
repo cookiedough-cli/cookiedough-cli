1b71c660b986229e40a1d4be7a83b9546e52ae42
abschill <ajay198810@gmail.com>
2022-06-05 21:34:20 -0600

    updates: config options+defaults

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,10 +2,10 @@
  * Runtime Types
  */
 export type InquirerPrompt = {
-	type   : string;
-	name   : string;
-	message: string;
-	choices: string[];
+	type   	: string;
+	name   	: string;
+	message	: string;
+	choices	: string[];
 }
 export type SystemOverview = {
 	arch     : string;
@@ -23,33 +23,42 @@ export type TemplateName =
 'python' ;
 
 export type PreloadedFileData = {
-	extension: string;
-	filename : string;
-	path     : string; //has to have <base> replaced with context when loaded as towrite w/ content
-	is_source: boolean;
+	extension		: string;
+	filename 		: string;
+	path     		: string; //has to have <base> replaced with context when loaded as towrite w/ content
+	is_source		: boolean;
 }
 
 export interface ToWriteFileData extends PreloadedFileData {
-	content ?: string;
+	content 	?: string;
 }
 
 export interface ProjectFileMap {
-	sys      : SystemOverview;
-	base_path: string;
-	add_tests: boolean;
-	files    ?: ToWriteFileData[];
+	sys      	: SystemOverview;
+	base_path	: string;
+	add_tests	: boolean;
+	files    	?: ToWriteFileData[];
 }
 
 export type Tuple = [string, string];
-export type Mapped<T> = {
-	[key: string]: T;
-};
+
+export type DizzyPathConfigOptions = {
+	out 				?: string; // base path to use to write new files during processing
+	process_root		?: string; // root of process scope
+	parent_path			?: string; // path of parent config to extend
+}
+
+export type DizzyProcessConfigOptions = {
+	detatched	?: boolean; // run in caller process or spawn its own
+	dry			?: boolean; // run without doing anything, just print the would-be output
+}
 
 export type DizzyConfigOptions = {
-	outPath     ?: string;
-	dryRun      ?: boolean;
-	template    ?: TemplateName;
-	customFiles ?: Mapped<string>;
+	path					?: DizzyPathConfigOptions; // path related config options
+	process					?: DizzyProcessConfigOptions; // runtime related config options
+	default_template    	?: TemplateName; // name of template to run against prompter
+	add_files_from			?: string[]; // directories to copy files into the new project from
+	always_use_prompt		?: boolean; // boolean whether to override settings default template in config
 }
 
 export type DizzyFilePrefix =
