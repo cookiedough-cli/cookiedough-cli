ed2b68b8abcf612f40edbdec0a665e07ac09bbc2
abschill <ajay198810@gmail.com>
2022-06-08 16:08:19 -0600

    chore: setup types/mjs options for submodules

--- cmd/index.ts
+++ cmd/index.ts
@@ -12,7 +12,27 @@ import {
  */
 export function useCookieDough() {
 	const recipe = useCMD(useGlobalConfig());
-	console.log(recipe);
+
+	switch(recipe.cmd) {
+		case 'locate':
+			console.log('todo: locate config root');
+			break;
+		case 'create-local-flavor':
+			console.log('todo: generate local flavor');
+			break;
+		case 'create':
+			console.log(recipe);
+			break;
+		case 'setup-env':
+			console.log('todo: setup root env');
+			break;
+		case 'edit':
+			console.log('todo: locate > open with default sys editor');
+			break;
+		case 'doctor':
+			console.log('todo: doctor callbacks');
+			break;
+	}
 	//  const config = useLocalConfig(args.url.parent_config);
 	//  useLog('found config:');
 	// useDataLog(config);
