cef8f9fa7ff4dcad89c16ea6be5793a32fd960ae
abschill <ajay198810@gmail.com>
2022-07-09 14:35:34 -0600

    chore:  arg parser - comment out most of old code

--- cmd/internal/cmd.ts
+++ cmd/internal/cmd.ts
@@ -23,44 +23,43 @@ Promise<CookieProcessRecipe> {
 		callback: useCreate
 	};
 	const inline = process.argv.slice(2);
+	// todo - set up new arg processor with promises setup
+	// if(inline.length > 0) {
+	// 	// command to run, validate it
+	// 	const cmd_list = CMDList.filter(cmd => cmd.signature === inline[0]);
+	// 	if(cmd_list.length === 1) {
+	// 		valid = cmd_list[0];
+	// 	}
+	// 	let crumbs;
 
-	// determine what the context of the command is
-	if(inline.length > 0) {
-		// command to run, validate it
-		const cmd_list = CMDList.filter(cmd => cmd.signature === inline[0]);
-		if(cmd_list.length === 1) {
-			valid = cmd_list[0];
-		}
-		let crumbs;
+	// 	if(inline.length > 1) {
+	// 		if(inline.includes('--no-config')) {
+	// 			useDefaultConfig().then(val => {
+	// 				crumbs = val;
+	// 			});
+	// 		}
+	// 		else if(inline.includes('-c') || inline.includes('--config')) {
+	// 			if(valid.signature === 'create' && !inline.includes('create')) {
+	// 				crumbs = useDirectoryConfig(inline[1] || '.');
+	// 			}
+	// 			else {
+	// 				crumbs = useDirectoryConfig(inline[2] || '.');
+	// 			}
+	// 		}
+	// 		else {
+	// 			crumbs = useGlobalConfigWithCWD();
+	// 		}
+	// 	}
 
-		if(inline.length > 1) {
-			if(inline.includes('--no-config')) {
-				useDefaultConfig().then(val => {
-					crumbs = val;
-				});
-			}
-			else if(inline.includes('-c') || inline.includes('--config')) {
-				if(valid.signature === 'create' && !inline.includes('create')) {
-					crumbs = useDirectoryConfig(inline[1] || '.');
-				}
-				else {
-					crumbs = useDirectoryConfig(inline[2] || '.');
-				}
-			}
-			else {
-				crumbs = useGlobalConfigWithCWD();
-			}
-		}
+	// 	const outData = {
+	// 		_raw_args: inline,
+	// 		_raw_cmd: cmd_list,
+	// 		cmd: valid,
+	// 		crumbs
+	// 	};
 
-		const outData = {
-			_raw_args: inline,
-			_raw_cmd: cmd_list,
-			cmd: valid,
-			crumbs
-		};
-
-		return outData;
-	}
+	// 	return outData;
+	// }
 	const crumbs = await useGlobalConfigWithCWD();
 	return {
 		_raw_args: inline,
