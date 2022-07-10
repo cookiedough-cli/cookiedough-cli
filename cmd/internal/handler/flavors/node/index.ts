c5f160d02f990177f31a75c76a403ea82c42531f
abschill <ajay198810@gmail.com>
2022-07-09 19:20:31 -0600

    chore: cleanup build preset setup

--- cmd/internal/handler/flavors/node/index.ts
+++ cmd/internal/handler/flavors/node/index.ts
@@ -1,26 +1,29 @@
-import { useNodeFlavorMap } from './pkg';
 import { useFlavorMod } from '../../..';
-import { join } from 'path';
 import inquirer from 'inquirer';
 import {
 	Tuple,
-	SystemOverview,
-	useLog,
 	useValidWritePath,
 	_call,
 	_callFrom,
 	useColor,
-	useFileList,
 	usePowerWasher,
 	useCopyMachine,
-	CrumbOptions
+	CrumbOptions,
+	ENV_RAW_SOURCE,
+	ENV_COOKIE_COPY_DIR,
+	useSysInfo,
+	SystemOverview
 } from '../..';
+import {
+	useNodeFlavorMap,
+	MappedNodeFlavor
+} from './pkg';
 export * from './modules';
 
 // get flavor from node json at root
 const NodeFlavor = Promise.resolve(useFlavorMod('node').then(flavor => flavor));
 // preset path to copy the files from depending on preferences
-const preset_path = 'https://raw.githubusercontent.com/cookiedough-cli/cookiedough-cli/main/.flavors/_copy_';
+const preset_path = `${ENV_RAW_SOURCE}${ENV_COOKIE_COPY_DIR}`;
 // user options for inquirer
 
 // the recipe to build as recieved from the user menu answers
@@ -33,118 +36,9 @@ export type NodeFlavorRecipe = {
 	eslint			: boolean;
 }
 
-// the next type created from the recipe for the specific context with paths / sys
-export interface NodeBuildInfo {
-	build_root			: string,
-	build_host			: SystemOverview,
-	build_preferences	: NodeFlavorRecipe,
-	build_frecipe		: MappedNodeFlavorRecipe
-}
 
-// setup the type for the installer + packages to install within for the shell prefix like yarn add vs npm install etc
-export type MappedNodeFlavorRecipe = {
-	installer: NodeModulePackager,
-	packages: NodeModule[]
-};
 // get the optional arg as a tuple of strings
 export type NodeModule = Tuple;
-
-// package manager type
-export type NodeModulePackager = {
-	name				: NodeFlavorPkg; //name of process to run
-	installSelf			: string; //command to install self if not installed / detected on system
-	installPkgSignature	: string; //prefix for adding packages between the process and the packagename
-}
-
-/**
- *
- * @param name
- * @param installPkgSignature
- * @returns
- */
-
-export function asNodeModulePackager(
-	name: NodeFlavorPkg,
-	installPkgSignature: string
-) : NodeModulePackager {
-	return <NodeModulePackager>{
-		name,
-		installSelf: '', //todo
-		installPkgSignature
-	}
-}
-
-export const NodeFlavor_PkgMgr = ['npm', 'yarn', 'pnpm'] as const;
-export type NodeFlavorPkg = typeof NodeFlavor_PkgMgr[number];
-
-export type NodeRecipeToFileMap = {
-	config: CrumbOptions,
-	recipe: NodeFlavorRecipe,
-	installer: NodeModulePackager,
-	packages: NodeModule[]
-}
-
-export function useFinalPresetCopy(
-	p: CrumbOptions,
-	node_build_info: NodeBuildInfo
-) {
-	if(!p.process.allow_cwd_write && (node_build_info.build_root == process.cwd())) {
-		useLog(`
-${useColor('yellow','warning:')}
-
-the path at ${node_build_info.build_root}\n is being compared as equal to the current working directory,
-which you have disabled writing to in your configuration
-if youd like to automatically override in the future, set:
-{
-	"process": {
-		"allow_cwd_write": true
-	}
-}
-
-in your config file.
-${useColor('yellow', 'exiting.')}`);
-		process.exit(0);
-	}
-	const preset_root = preset_path + '/';
-
-	useCopyMachine(join(preset_root, 'default'), node_build_info.build_root);
-	useCopyMachine(join(preset_root, `node/*/${node_build_info.build_preferences.preset}`), node_build_info.build_root);
-	if(node_build_info.build_preferences.eslint) {
-		useCopyMachine(join(preset_root, 'node/eslint'), node_build_info.build_root);
-	}
-	if(node_build_info.build_preferences.bundler === 'webpack') {
-		useCopyMachine(join(preset_root, 'node/bundler/webpack'), node_build_info.build_root);
-	}
-	if(node_build_info.build_preferences.bundler === 'rollup') {
-		useCopyMachine(join(preset_root, `node/bundler/rollup/${node_build_info.build_preferences.preset}`), node_build_info.build_root);
-	}
-	if(node_build_info.build_preferences.bundler === 'swcpack') {
-		useCopyMachine(join(preset_root, `node/bundler/swcpack/${node_build_info.build_preferences.preset}`), node_build_info.build_root);
-	}
-
-	if(node_build_info.build_preferences.compiler === 'swc') {
-		useCopyMachine(join(preset_root, `node/compiler/swc/${node_build_info.build_preferences.preset}`), node_build_info.build_root);
-	}
-}
-
-export function useNodeInstaller(
-	p: CrumbOptions,
-	node_build_info: NodeBuildInfo
-) {
-	console.log(p);
-	console.log(node_build_info);
-	// const install_cmd = `${p.process.shell_prefix ?? ''}${node_build_info.build_frecipe.installer.name} init -y && ${node_build_info.build_frecipe.installer.name} ${node_build_info.build_frecipe.installer.installPkgSignature}`;
-	// const install_dev_list = node_build_info.build_frecipe.packages.filter(pkg => pkg[1] === '-D').map(pkg => pkg[0]).join(' ');
-	// _callFrom(node_build_info.build_root, `${install_cmd} -D ${install_dev_list}`);
-	// if(p.process.add_files_from) {
-	// 	const filesToCopy = p.process.add_files_from.map(filePath => useFileList(filePath));
-	// 	if(filesToCopy.flat().length > 0) {
-	// 		for(const dir of p.process.add_files_from) {
-	// 			useCopyMachine(dir, node_build_info.build_root);
-	// 		}
-	// 	}
-	// }
-}
 
 /**
  * Flavor runtime file MUST have a root usePrompt to work properly with the CLI and it must return a noop, throw anything bad that happens else its considered successful
@@ -152,13 +46,21 @@ export function useNodeInstaller(
  * @returns no op cli process
  */
 
-export async function usePrompt<NodeFlavorRecipe>(
+export async function usePrompt(
 	p: CrumbOptions,
-): Promise<NodeFlavorRecipe> {
+): Promise<{
+	_sys: SystemOverview,
+	crumbs: CrumbOptions,
+	flavor: MappedNodeFlavor
+}> {
 	const NodeUserOptions = (await NodeFlavor).doughmap;
-	const answers: NodeFlavorRecipe = await inquirer.prompt(NodeUserOptions);
-	// console.log(answers);
-	return answers;
+	const answers = await inquirer.prompt(NodeUserOptions);
+	const flavor = useNodeFlavorMap(answers);
+	return {
+		_sys: useSysInfo(),
+		crumbs: p,
+		flavor
+	};
 // 		const ppm = useNodeFlavorMap(answers);
 // 		const node_build_info: NodeBuildInfo = {
 // 			build_root: p.path.out,
--- cmd/internal/handler/flavors/node/pkg.ts
+++ cmd/internal/handler/flavors/node/pkg.ts
@@ -1,4 +1,8 @@
 import {
+	CrumbOptions,
+	SystemOverview
+ } from '../..';
+import {
 	NodeFlavorRecipe,
 	NodeModule,
 	SWCBaseModules,
@@ -12,15 +16,51 @@ import {
 	RollupModules,
 	RollupTSModules,
 	GulpTSModules,
-	NodeModulePackager,
-	asNodeModulePackager,
-	MappedNodeFlavorRecipe,
 	SWCPackModules
 } from '.';
 
+// package manager type
+export type NodeModulePackager = {
+	name				: NodeFlavorPkg; //name of process to run
+	installSelf			: string; //command to install self if not installed / detected on system
+	installPkgSignature	: string; //prefix for adding packages between the process and the packagename
+}
+export const NodeFlavor_PkgMgr = ['npm', 'yarn', 'pnpm'] as const;
+export type NodeFlavorPkg = typeof NodeFlavor_PkgMgr[number];
+
+// setup the type for the installer + packages to install within for the shell prefix like yarn add vs npm install etc
+export type MappedNodeFlavor = {
+	installer: NodeModulePackager;
+	node_modules: NodeModule[];
+};
+export type NodeRecipeToFileMap = {
+	config: CrumbOptions;
+	recipe: NodeFlavorRecipe;
+	installer: NodeModulePackager;
+	packages: NodeModule[];
+}
+
+/**
+ *
+ * @param name
+ * @param installPkgSignature
+ * @returns
+ */
+
+ export function asNodeModulePackager(
+	name: NodeFlavorPkg,
+	installPkgSignature: string
+) : NodeModulePackager {
+	return <NodeModulePackager>{
+		name,
+		installSelf: '', //todo
+		installPkgSignature
+	}
+}
+
 export const useNodeFlavorMap = (
 	np: NodeFlavorRecipe
-): MappedNodeFlavorRecipe => {
+): MappedNodeFlavor => {
 	let installer: NodeModulePackager;
 
 	switch(np.pkg_mgr) {
@@ -98,7 +138,76 @@ export const useNodeFlavorMap = (
 	// return package list
 	return {
 		installer,
-		packages: needsPackage
+		node_modules: needsPackage
 	};
 };
 
+// export function useFinalPresetCopy(
+// 	p: CrumbOptions,
+// 	node_build_info: NodeBuildInfo
+// ) {
+// 	if(!p.process.allow_cwd_write && (node_build_info.build_root == process.cwd())) {
+// 		useLog(`
+// ${useColor('yellow','warning:')}
+
+// the path at ${node_build_info.build_root}\n is being compared as equal to the current working directory,
+// which you have disabled writing to in your configuration
+// if youd like to automatically override in the future, set:
+// {
+// 	"process": {
+// 		"allow_cwd_write": true
+// 	}
+// }
+
+// in your config file.
+// ${useColor('yellow', 'exiting.')}`);
+// 		process.exit(0);
+// 	}
+// 	const preset_root = preset_path + '/';
+
+// 	useCopyMachine(join(preset_root, 'default'), node_build_info.build_root);
+// 	useCopyMachine(join(preset_root, `node/*/${node_build_info.build_preferences.preset}`), node_build_info.build_root);
+// 	if(node_build_info.build_preferences.eslint) {
+// 		useCopyMachine(join(preset_root, 'node/eslint'), node_build_info.build_root);
+// 	}
+// 	if(node_build_info.build_preferences.bundler === 'webpack') {
+// 		useCopyMachine(join(preset_root, 'node/bundler/webpack'), node_build_info.build_root);
+// 	}
+// 	if(node_build_info.build_preferences.bundler === 'rollup') {
+// 		useCopyMachine(join(preset_root, `node/bundler/rollup/${node_build_info.build_preferences.preset}`), node_build_info.build_root);
+// 	}
+// 	if(node_build_info.build_preferences.bundler === 'swcpack') {
+// 		useCopyMachine(join(preset_root, `node/bundler/swcpack/${node_build_info.build_preferences.preset}`), node_build_info.build_root);
+// 	}
+
+// 	if(node_build_info.build_preferences.compiler === 'swc') {
+// 		useCopyMachine(join(preset_root, `node/compiler/swc/${node_build_info.build_preferences.preset}`), node_build_info.build_root);
+// 	}
+// }
+
+// export function useNodeInstaller(
+// 	p: CrumbOptions,
+// 	node_build_info: NodeBuildInfo
+// ) {
+// 	console.log(p);
+// 	console.log(node_build_info);
+// 	const install_cmd = `${p.process.shell_prefix ?? ''}${node_build_info.build_frecipe.installer.name} init -y && ${node_build_info.build_frecipe.installer.name} ${node_build_info.build_frecipe.installer.installPkgSignature}`;
+// 	const install_dev_list = node_build_info.build_frecipe.packages.filter(pkg => pkg[1] === '-D').map(pkg => pkg[0]).join(' ');
+// 	return {
+// 		crumb_options: p,
+// 		build_info: node_build_info,
+// 		install: {
+// 			cmd: install_cmd,
+// 			dev_pkg: install_dev_list
+// 		}
+// 	};
+// 	// _callFrom(node_build_info.build_root, `${install_cmd} -D ${install_dev_list}`);
+// 	// if(p.process.add_files_from) {
+// 	// 	const filesToCopy = p.process.add_files_from.map(filePath => useFileList(filePath));
+// 	// 	if(filesToCopy.flat().length > 0) {
+// 	// 		for(const dir of p.process.add_files_from) {
+// 	// 			useCopyMachine(dir, node_build_info.build_root);
+// 	// 		}
+// 	// 	}
+// 	// }
+// }
